54a523d0466d63c64419760e233afbd96df8c5df
Konboi <ryosuke.yabuki@gmail.com>
2015-05-18 00:04:41 +0900

    Add 01

---
+++ 01/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	str := "stressed"
+
+	fmt.Printf("before: %s \n", str)
+
+	reverseStr := Reverse(str)
+
+	fmt.Printf("after:  %s \n", reverseStr)
+
+}
+
+func Reverse(s string) string {
+	runes := []rune(s)
+
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+
+	return string(runes)
+}
