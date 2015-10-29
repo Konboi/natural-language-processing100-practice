1704c2c9ccb2261b3a4102ccceb79ddb5c0b9b4a
mackee <macopy123@gmail.com>
2015-10-29 17:06:12 +0900

    channelを使わないバージョンとの比較

--- 13/main.go
+++ 13/main.go
@@ -14,16 +14,14 @@ const (
 	MERGED_FILENAME = "merged.txt"
 )
 
-var (
-	ch1 = make(chan string, 256)
-	ch2 = make(chan string, 256)
-)
-
 func main() {
-	MergeColumnsWithChannel()
+	MergeColumns()
 }
 
 func MergeColumnsWithChannel() {
+	ch1 := make(chan string, 256)
+	ch2 := make(chan string, 256)
+
 	f1, err := os.Open(COL1_FILENAME)
 	if err != nil {
 		log.Fatalln("open col1.txt error:", err)
@@ -63,4 +61,31 @@ func ScanFile(r io.Reader, ch chan string) {
 		ch <- t
 	}
 	close(ch)
+}
+
+func MergeColumns() {
+	f1, err := os.Open(COL1_FILENAME)
+	if err != nil {
+		log.Fatalln("open col1.txt error:", err)
+	}
+	defer f1.Close()
+	s1 := bufio.NewScanner(f1)
+	f2, err := os.Open(COL2_FILENAME)
+	if err != nil {
+		log.Fatalln("open col2.txt error:", err)
+	}
+	defer f2.Close()
+	s2 := bufio.NewScanner(f2)
+
+	mf, err := os.Create(MERGED_FILENAME)
+	if err != nil {
+		log.Fatalln("create merged.txt error:", err)
+	}
+	defer mf.Close()
+
+	for s1.Scan() && s2.Scan() {
+		c1 := s1.Text()
+		c2 := s2.Text()
+		fmt.Fprintf(mf, "%s\t%s\n", c1, c2)
+	}
 }
